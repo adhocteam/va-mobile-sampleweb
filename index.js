b90b0bfbace92953b2bf14a9fddbf89f9451d212
Kris Pethtel <kris.pethtel@gmail.com>
2022-07-07 15:00:34 -0400

    update to new metadata and jwks endpoints

--- index.js
+++ index.js
@@ -20,10 +20,10 @@ const CALLBACK_URL = process.env.CALLBACK_URL || 'http://localhost:' + PORT + '/
 function createClient() {
   Issuer.defaultHttpOptions = { timeout: 5000 };
   const ssoeIssuer = new Issuer({
-    issuer: 'https://sqa.fed.eauth.va.gov/oauthe/sps/oauth/oauth20/metadata/ISAMOPe',
+    issuer: 'https://sqa.fed.eauth.va.gov/oauthe/sps/oauth/oauth20/metadata/ISAMOPeFP',
     authorization_endpoint: OAUTH_URL,
     token_endpoint: 'https://sqa.fed.eauth.va.gov/oauthe/sps/oauth/oauth20/token',
-    jwks_uri: 'https://sqa.fed.eauth.va.gov/oauthe/sps/oauth/oauth20/jwks/ISAMOPe',
+    jwks_uri: 'https://sqa.fed.eauth.va.gov/oauthe/sps/oauth/oauth20/jwks/ISAMOPeFP',
     //Advertised in  metadata but seemingly not supported
     // userinfo_endpoint: 'https://sqa.fed.eauth.va.gov/oauthi/sps/oauth/oauth20/userinfo',
   });

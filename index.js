b6f4dfee9ced12180940e4971b548e64fe58d137
Kris Pethtel <kris.pethtel@gmail.com>
2022-07-27 13:54:21 -0400

    db issues

--- index.js
+++ index.js
@@ -181,7 +181,7 @@ function startApp(client) {
   app.get('/auth/login-success', passport.authenticate('oidc'),
     async function(req, res) {
       console.log('ASSIGNING USER')
-      db.connect();
+      await db.connect();
 
       req.session.user = Object.assign(req.user);
 
@@ -191,7 +191,6 @@ function startApp(client) {
       const refreshToken = req.session.user.refresh_token;
 
       const { rows } = await db.query('SELECT * FROM tokens WHERE email = $1 LIMIT 1', [email])
-      db.end();
       const record = rows[0]
 
       console.log('FOUND RECORD: ', record)
@@ -210,7 +209,7 @@ function startApp(client) {
       console.log('STATEMENT', statement)
       console.log('VALUES', values)
 
-      db.connect();
+      await db.connect();
       db.query(statement, values, (err, res) => {
         console.log('INSERT/UPDATE RESPONSE', res)
         if (err) throw err;
